d90de2d163f7e1d15b972776d2183fb61cce5947
ResidentTenko <109637465+ResidentTenko@users.noreply.github.com>
2024-07-23 17:04:54 -0400

    Adds project backend file.

---
+++ RevLearn/src/main/java/com/revature/RevLearn/RevLearnApplication.java
@@ -0,0 +1,13 @@
+package com.revature.RevLearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevLearnApplication.class, args);
+	}
+
+}
