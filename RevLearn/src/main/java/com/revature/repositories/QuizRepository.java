c187fb18ab63ec62c31679fcc57882129acf1a0a
ejlambert <eric.j.lambert@gmail.com>
2024-07-24 12:42:47 -0400

    fixed quiz repo

--- RevLearn/src/main/java/com/revature/repositories/QuizRepository.java
+++ RevLearn/src/main/java/com/revature/repositories/QuizRepository.java
@@ -1,7 +1,9 @@
 package com.revature.repositories;
 
+import com.revature.models.Quiz;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface QuizRepository {
+public interface QuizRepository extends JpaRepository<Quiz, Integer> {
 }
