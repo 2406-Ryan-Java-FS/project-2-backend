fb7efc5a7241df15bf4743552470f32bcfa7d099
Teddy Solomon <xzl896@mocs.utc.edu>
2024-07-24 16:54:47 -0500

    Adding repos

---
+++ RevLearn/src/main/java/com/revature/repositories/QuestionChoiceRepo.java
@@ -0,0 +1,12 @@
+package com.revature.repositories;
+
+import com.revature.models.QuestionChoice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface QuestionChoiceRepo extends JpaRepository<QuestionChoice, Integer>{
+    List<QuestionChoice> findByQuestionId(int questionId);
+}
