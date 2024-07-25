bb24b737e6840354debcf0d5b47bc2b0b0de496c
c-Harrod <101145026+c-Harrod@users.noreply.github.com>
2024-07-25 17:43:41 -0400

    adds patch request mapping to update payment_status

--- RevLearn/src/main/java/com/revature/controllers/EnrollmentController.java
+++ RevLearn/src/main/java/com/revature/controllers/EnrollmentController.java
@@ -1,13 +1,16 @@
 package com.revature.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.exceptions.BadRequestException;
 import com.revature.models.Enrollment;
+import com.revature.models.PayStatus;
 import com.revature.services.EnrollmentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
@@ -35,6 +38,27 @@ public class EnrollmentController {
         } catch (BadRequestException e){
            return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
 
+
+    @PatchMapping("/enrollments/{id}")
+    public ResponseEntity<?> updatePaymentStatusForEnrollment(@PathVariable("id") Integer theEnrollmentId, @RequestBody String payStatus){
+
+        try{
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(payStatus);
+            String payStatusString = jsonNode.get("payStatus").asText();
+
+            PayStatus status = PayStatus.valueOf(payStatusString);
+
+            return ResponseEntity.ok(enrollmentService.updateEnrollmentById(theEnrollmentId, status));
+
+        } catch (JsonMappingException e) {
+            throw new BadRequestException("Could not complete update request");
+        } catch (JsonProcessingException e) {
+            throw new BadRequestException("Could not complete update request");
+        } catch (IllegalArgumentException e){
+            throw new BadRequestException("Please enter 'pending', 'completed', or 'cancelled'");
+        }
     }
 }
