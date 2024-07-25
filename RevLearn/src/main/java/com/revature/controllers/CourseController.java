644cacc0104f12d5ceccabe885471ffec7af0dad
c-Harrod <101145026+c-Harrod@users.noreply.github.com>
2024-07-25 16:56:04 -0400

    adds constructor injection to Course model

--- RevLearn/src/main/java/com/revature/controllers/CourseController.java
+++ RevLearn/src/main/java/com/revature/controllers/CourseController.java
@@ -1,8 +1,17 @@
 package com.revature.controllers;
 
+
+import com.revature.services.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CourseController {
-    
+
+    CourseService courseService;
+
+    @Autowired
+    public CourseController( CourseService courseService){
+        this.courseService = courseService;
+    }
 }
