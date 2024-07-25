ee9a0f6b1210f7998a1c96841eca7dc97c2ce341
ResidentTenko <109637465+ResidentTenko@users.noreply.github.com>
2024-07-25 17:59:55 -0400

    Adds getCourseById

--- RevLearn/src/main/java/com/revature/services/CourseServiceImpl.java
+++ RevLearn/src/main/java/com/revature/services/CourseServiceImpl.java
@@ -1,9 +1,11 @@
 package com.revature.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.revature.exceptions.NotFoundException;
 import com.revature.models.Course;
 import com.revature.repositories.CourseRepository;
 
@@ -27,12 +29,26 @@ public class CourseServiceImpl implements CourseService{
         throw new UnsupportedOperationException("Unimplemented method 'addCourse'");
     }
 
+    /**
+     * retrieves a course entitiy from the repository
+     * @param theCourseId the id of the course we want to find
+     * @return a course object if it exists in the database
+     * @throws NotFoundException if the course does not exist
+     */
     @Override
     public Course getCourseById(Integer theCourseId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getCourseById'");
-    }
 
+        Optional<Course> dBCourse = courseRepository.findById(theCourseId);
+
+        if (dBCourse.isPresent()) 
+        {
+            return dBCourse.get();
+        } 
+        else 
+        {
+            throw new NotFoundException("Course does not exist. Please check the course ID: " + theCourseId);
+        }
+    }
 
     /**
      *  This method takes in an int to query the Courses in the database and return a list of all courses with the matching
@@ -42,7 +58,6 @@ public class CourseServiceImpl implements CourseService{
      */
     @Override
     public List<Course> getCoursesByEducatorId(Integer theEducatorId) {
-
         return courseRepository.findByEducatorId(theEducatorId);
     }
 
