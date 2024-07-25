d5190475b0985ef0831cc33f9e46349dda1470ac
patrickdenert9 <patrickdenert@gmail.com>
2024-07-25 14:26:38 -0400

    updated models to use enums

--- RevLearn/src/main/java/com/revature/models/QuizQuestion.java
+++ RevLearn/src/main/java/com/revature/models/QuizQuestion.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
-@Table(name = "quizquestions")
+@Table(name = "quizquestions", schema = "project2")
 @Data
 public class QuizQuestion {
 
