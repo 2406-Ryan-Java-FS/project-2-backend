c7c37f13cc0ce82a3ca255f52deb04679d12c237
c-Harrod <101145026+c-Harrod@users.noreply.github.com>
2024-07-25 17:04:45 -0400

    Update EnrollmentController.java
    adds a GET request handler to retrieve an Enrollment record by its Id

--- RevLearn/src/main/java/com/revature/controllers/EnrollmentController.java
+++ RevLearn/src/main/java/com/revature/controllers/EnrollmentController.java
@@ -1,8 +1,40 @@
 package com.revature.controllers;
 
+import com.revature.exceptions.BadRequestException;
+import com.revature.models.Enrollment;
+import com.revature.services.EnrollmentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class EnrollmentController {
-    
+
+    EnrollmentService enrollmentService;
+
+    public EnrollmentController(EnrollmentService enrollmentService){
+        this.enrollmentService = enrollmentService;
+    }
+
+
+
+
+
+
+
+    @GetMapping("/enrollments/{id}")
+    public ResponseEntity<?> getEnrollmentById(@PathVariable("id") Integer theEnrollmentId){
+
+        try{
+            Enrollment theEnrollment = enrollmentService.getEnrollmentById(theEnrollmentId);
+
+            return ResponseEntity.ok(theEnrollment);
+        } catch (BadRequestException e){
+           return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
+    }
 }
