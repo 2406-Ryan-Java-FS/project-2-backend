5068ba3b15fc260647ff95c8316ec264a76fe4b9
Victor Galbraith <victorg87@gmail.com>
2024-07-26 11:47:55 -0700

    Updated endpoints to catch exceptions and pass along relevant message and Http status code

--- RevLearn/src/main/java/com/revature/controllers/UserController.java
+++ RevLearn/src/main/java/com/revature/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.revature.controllers;
 
 import com.revature.DTO.UserEducatorDTO;
+import com.revature.exceptions.CustomHttpException;
 import com.revature.exceptions.UnauthorizedException;
 import com.revature.models.Educator;
 import com.revature.models.User;
@@ -32,27 +33,31 @@ public class UserController {
      * Endpoint for registering a new User.
      *
      * @param userEducatorDTO Data containing User fields and optionally Educator fields to be registered.
-     * @return A ResponseEntity containing the userId of the newly persisted User, or the educatorId if the User is an Educator, along with a 201 status code.
+     * @return A ResponseEntity containing the userId of the newly persisted User, or the educatorId if the User is an Educator, along with a 201 status code on success, or an error message and appropriate status code on failure.
      */
     @PostMapping
     public ResponseEntity<Object> addUser(@RequestBody UserEducatorDTO userEducatorDTO) {
 
-        User newUser = userService.addUser(userEducatorDTO.extractUser());
+        try {
+            User newUser = userService.addUser(userEducatorDTO.extractUser());
 
-        if (newUser.getRole().equals(Role.educator)) {
-            Educator extractedEducator = userEducatorDTO.extractEducator();
-            extractedEducator.setEducatorId(newUser.getUserId());
-            Educator newEducator = educatorService.addEducator(extractedEducator);
-            return new ResponseEntity<>(newEducator.getEducatorId(), HttpStatus.CREATED);
+            if (newUser.getRole().equals(Role.educator)) {
+                Educator extractedEducator = userEducatorDTO.extractEducator();
+                extractedEducator.setEducatorId(newUser.getUserId());
+                Educator newEducator = educatorService.addEducator(extractedEducator);
+                return new ResponseEntity<>(newEducator.getEducatorId(), HttpStatus.CREATED);
+            }
+            return new ResponseEntity<>(newUser.getUserId(), HttpStatus.CREATED);
+        } catch (CustomHttpException e) {
+            return new ResponseEntity<>(e.getMessage(), e.getStatus());
         }
-        return new ResponseEntity<>(newUser.getUserId(), HttpStatus.CREATED);
     }
 
     /**
      * Endpoint for verifying a User login.
      *
-     * @param user A User containing a email/password combination to be verified.
-     * @return A JWT Token as a string.
+     * @param user A User containing an email/password combination to be verified.
+     * @return A ResponseEntity containing a JWT Token as a string along with a 200 status code on success, or an error message and appropriate status code on failure.
      */
     @PostMapping("/login")
     public ResponseEntity<Object> loginUser(@RequestBody User user) {
@@ -60,8 +65,8 @@ public class UserController {
         try {
             Integer validUserId = userService.verifyUser(user);
             return new ResponseEntity<>(jwtService.generateJwt(validUserId), HttpStatus.OK);
-        } catch (UnauthorizedException ue) {
-            return new ResponseEntity<>(ue.getMessage(), HttpStatus.UNAUTHORIZED);
+        } catch (CustomHttpException e) {
+            return new ResponseEntity<>(e.getMessage(), e.getStatus());
         }
     }
 
@@ -69,18 +74,22 @@ public class UserController {
      * Endpoint for retrieving the currently logged-in User and Educator information given a valid JWT authorization.
      *
      * @param authorization The JWT authorization token of the currently logged-in User.
-     * @return A ResponseEntity containing the User and Educator data (if applicable) along with a 200 status code.
+     * @return A ResponseEntity containing the User and Educator data (if applicable) along with a 200 status code on success, or an error message and appropriate status code on failure.
      */
     @GetMapping
     public ResponseEntity<Object> getCurrentUser(@RequestHeader String authorization) {
 
-        User currentUser = jwtService.getUserFromToken(authorization);
+        try {
+            User currentUser = jwtService.getUserFromToken(authorization);
 
-        if (currentUser.getRole().equals(Role.educator)) {
-            Educator currentEducator = educatorService.getEducator(currentUser.getUserId());
-            UserEducatorDTO dto = userService.combineUserAndEducator(currentUser, currentEducator);
-            return new ResponseEntity<>(dto, HttpStatus.OK);
+            if (currentUser.getRole().equals(Role.educator)) {
+                Educator currentEducator = educatorService.getEducator(currentUser.getUserId());
+                UserEducatorDTO dto = userService.combineUserAndEducator(currentUser, currentEducator);
+                return new ResponseEntity<>(dto, HttpStatus.OK);
+            }
+            return new ResponseEntity<>(currentUser, HttpStatus.OK);
+        } catch (CustomHttpException e) {
+            return new ResponseEntity<>(e.getMessage(), e.getStatus());
         }
-        return new ResponseEntity<>(currentUser, HttpStatus.OK);
     }
 }
