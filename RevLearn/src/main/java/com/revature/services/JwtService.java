be89c8fb636c5b261accd8309d59c3167e06dbcc
Hiben <yachibed@amazon.com>
2024-07-26 12:30:39 -0500

    moved

--- RevLearn/src/main/java/com/revature/services/JwtService.java
+++
@@ -1,9 +0,0 @@
-package com.revature.services;
-
-import com.revature.models.Users;
-
-public interface JwtService {
-    public String generateJwt(Integer id);
-    public Users getUserFromToken(String token);
-    public int verifyJwt(String token);
-}
