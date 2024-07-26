fc636d7bfa0065ecfd62599a942364ef22ba788b
Victor Galbraith <victorg87@gmail.com>
2024-07-26 07:27:34 -0700

    incorporated use of JWT token to GET current user from /educators endpoint

--- RevLearn/src/main/java/com/revature/controllers/EducatorController.java
+++ RevLearn/src/main/java/com/revature/controllers/EducatorController.java
@@ -1,7 +1,9 @@
 package com.revature.controllers;
 
 import com.revature.models.Educator;
+import com.revature.models.User;
 import com.revature.services.EducatorService;
+import com.revature.services.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,10 +14,12 @@ import org.springframework.web.bind.annotation.*;
 public class EducatorController {
 
     EducatorService educatorService;
+    JwtService jwtService;
 
     @Autowired
-    public EducatorController(EducatorService educatorService) {
+    public EducatorController(EducatorService educatorService, JwtService jwtService) {
         this.educatorService = educatorService;
+        this.jwtService = jwtService;
     }
 
     /**
@@ -31,16 +35,30 @@ public class EducatorController {
         return new ResponseEntity<>(newEducator, HttpStatus.CREATED);
     }
 
-    /**
-     * Endpoint for retrieving an Educator given its educatorId.
-     *
-     * @param educatorId The educatorId of the Educator to retrieve.
-     * @return The Educator along with a 200 status code.
-     */
-    @GetMapping("/{educatorId}")
-    public ResponseEntity<Educator> getEducator(@PathVariable Integer educatorId) {
+//    /**
+//     * Endpoint for retrieving an Educator given its educatorId.
+//     *
+//     * @param educatorId The educatorId of the Educator to retrieve.
+//     * @return The Educator along with a 200 status code.
+//     */
+//    @GetMapping("/{educatorId}")
+//    public ResponseEntity<Educator> getEducator(@PathVariable Integer educatorId) {
+//
+//        Educator existingEducator = educatorService.getEducator(educatorId);
+//        return new ResponseEntity<>(existingEducator, HttpStatus.OK);
+//    }
 
-        Educator existingEducator = educatorService.getEducator(educatorId);
-        return new ResponseEntity<>(existingEducator, HttpStatus.OK);
+    /**
+     * Endpoint for retrieving the currently logged-in Educator given a valid JWT token.
+     *
+     * @param token The JWT token of the currently logged-in Educator.
+     * @return The User along with a 200 status code.
+     */
+    @GetMapping
+    public ResponseEntity<Educator> getCurrentEducator(@PathVariable String token) {
+
+        User currentUser = jwtService.getUserFromToken(token);
+        Educator currentEducator = educatorService.getEducator(currentUser.getUserId());
+        return new ResponseEntity<>(currentEducator, HttpStatus.OK);
     }
 }
