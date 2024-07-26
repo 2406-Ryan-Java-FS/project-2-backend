e5be7c0226a36db8547bba9f0b2eec7a51fdc56e
ResidentTenko <109637465+ResidentTenko@users.noreply.github.com>
2024-07-26 11:03:25 -0400

    Fixed missing brackets

--- RevLearn/src/main/java/com/revature/controllers/EnrollmentController.java
+++ RevLearn/src/main/java/com/revature/controllers/EnrollmentController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -124,7 +125,7 @@ public class EnrollmentController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
     }
-}
+
 
     @GetMapping("/enrollments/{studentId}")
     public ResponseEntity<List<Enrollment>> getEnrollmentByStudentId(@PathVariable("studentId") Integer theStudentId) {
