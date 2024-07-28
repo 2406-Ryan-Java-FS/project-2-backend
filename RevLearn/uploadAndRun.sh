6190bb55846f50fa560094ea95557ad78f4168de
mrCoutureLocal <>
2024-07-27 20:59:29 -0400

    Installed mvn and java on my ec2. Wrote uploadAndRun script to automate deployment.

---
+++ RevLearn/uploadAndRun.sh
@@ -0,0 +1,36 @@
+
+set -e
+
+echo "AWS_EC2_IP=$AWS_EC2_IP"
+
+echo "Shell script: Uploading src code files"
+sftp -i ~/project2.pem ec2-user@$AWS_EC2_IP << 'END'
+  put pom.xml
+  put -r src
+END
+
+#echo "Shell script: Uploading jar file"
+#sftp -i ~/project2.pem ec2-user@$AWS_EC2_IP << 'END'
+#  put target/RevLearn-0.0.1-SNAPSHOT.jar
+#END
+
+ssh -i ~/project2.pem ec2-user@$AWS_EC2_IP << 'END'
+  echo "Shell script: Compiling using maven"
+  mvn package -DskipTests
+END
+
+
+ssh -i ~/project2.pem ec2-user@$AWS_EC2_IP << 'END'
+  echo "Shell script: Killing the running jar"
+  sudo pkill -f RevLearn-0.0.1-SNAPSHOT.jar
+END
+
+echo "Shell script: Waiting for the jar to stop"
+sleep 6
+
+ssh -i ~/project2.pem ec2-user@$AWS_EC2_IP << 'END'
+  echo "Shell script: Starting the new jar"
+  sudo java -jar target/RevLearn-0.0.1-SNAPSHOT.jar --spring.profiles.active=h2 --server.port=80
+END
+
+#sudo nohup java -jar target/RevLearn-0.0.1-SNAPSHOT.jar --spring.profiles.active=h2 --server.port=80 > theOutput.txt 2>&1 &
---
+++ RevLearn/yumInstallOnEc2.sh
@@ -0,0 +1,8 @@
+
+
+
+sudo yum install docker -y
+
+sudo yum install commons-compiler-jdk.noarch
+
+sudo yum install maven-local-amazon-corretto21.noarch
