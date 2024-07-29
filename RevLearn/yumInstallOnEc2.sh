5ba0b8d85c4784ee2bc063e43a0131836f359639
mrCoutureLocal <>
2024-07-29 19:01:15 -0400

    Setup nginx as a gateway. documenting commands.

--- RevLearn/yumInstallOnEc2.sh
+++ RevLearn/yumInstallOnEc2.sh
@@ -1,5 +1,5 @@
 
-
+yum search theThingYouNeed
 
 sudo yum install docker -y
 
@@ -8,7 +8,30 @@ sudo yum install commons-compiler-jdk.noarch
 sudo yum install maven-local-amazon-corretto21.noarch
 
 
-curl https://downloads.apache.org/kafka/3.7.0/kafka_2.12-3.7.0.tgz -o kafka_2.12-3.7.0.tgz
+sudo yum install nginx.x86_64
+cd /etc/nginx/                      #Where yum installed this
+
+sudo systemctl start nginx
+
+#Crucial we create this /etc/nginx/default.d/myLocations.conf
+#There will be awful file permissions errors otherwise
+sudo nano myLocations.conf
+location /project-2-back/ {
+        proxy_pass http://localhost:8080/;
+}
+
+sudo nginx -t
+sudo nginx -T
+sudo nginx -s reload
+
+cd /usr/share/nginx/
+sudo chown -R ec2-user:ec2-user html/
+/usr/share/nginx/html   #default location where static files are served
+
+
+
+
+curl -O https://downloads.apache.org/kafka/3.7.0/kafka_2.12-3.7.0.tgz
 tar -xf kafka_2.12-3.7.0.tgz
 
 #was trying to change kafka memory settings. didn't work.
