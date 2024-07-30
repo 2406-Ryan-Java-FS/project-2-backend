48a39e6c7759bd8a4811c397739bc8a240177436
Victor Galbraith <victorg87@gmail.com>
2024-07-30 07:25:26 -0700

    minor formatting changes

--- RevLearn/src/main/java/com/revature/exceptions/CustomHttpException.java
+++ RevLearn/src/main/java/com/revature/exceptions/CustomHttpException.java
@@ -5,11 +5,11 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public abstract class CustomHttpException extends RuntimeException {
+
     private final HttpStatus status;
 
     protected CustomHttpException(String message, HttpStatus status) {
         super(message);
         this.status = status;
     }
-
 }
