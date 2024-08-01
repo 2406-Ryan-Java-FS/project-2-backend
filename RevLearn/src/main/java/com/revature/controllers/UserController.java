ba04a218aa9068935bb0100f62fe11f386bc8a63
Victor Galbraith <victorg87@gmail.com>
2024-08-01 08:59:05 -0700

    added @CrossOrigin to UserController

--- RevLearn/src/main/java/com/revature/controllers/UserController.java
+++ RevLearn/src/main/java/com/revature/controllers/UserController.java
@@ -15,6 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/users")
 public class UserController {
 
