2c4ee279c0e51486298b43af490a7f10e2deeeae
ejlambert <eric.j.lambert@gmail.com>
2024-08-01 10:47:40 -0400

    added crossOrigin to quiz controller

--- RevLearn/src/main/java/com/revature/controllers/QuizController.java
+++ RevLearn/src/main/java/com/revature/controllers/QuizController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = " http://localhost:3000 ")
 public class QuizController {
 
     QuizServiceImpl qs;
